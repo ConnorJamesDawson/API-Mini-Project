5b0c88514579c4fa8869c3196d8f0ef20dfd8d60
Byron Esson <byronesson767@gmail.com>
2023-04-04 10:47:14 +0100

    complete products controller tests

--- NorthwindAPI-MiniProject/NorthwindAPI.Tests/ProductsControllerShould.cs
+++ NorthwindAPI-MiniProject/NorthwindAPI.Tests/ProductsControllerShould.cs
@@ -1,4 +1,5 @@
 ﻿using Castle.Core.Logging;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -55,7 +56,7 @@ internal class ProductsControllerShould
         Mock
         .Get(mockService)
         .Setup(sc => sc.GetAllAsync().Result)
-        .Returns((List<Product>)null);
+        .Returns<Task>(null);
 
         var sut = new ProductsController(mockService);
         var result = await sut.GetProducts();
@@ -79,16 +80,10 @@ internal class ProductsControllerShould
             }
         };
 
-
-
         Mock
         .Get(mockService)
         .Setup(sc => sc.GetAsync(1).Result)
         .Returns(products[0]);
-
-
-
-
 
         var sut = new ProductsController(mockService);
         var result = await sut.GetProduct(1);
@@ -131,6 +126,23 @@ internal class ProductsControllerShould
         Assert.That(result, Is.InstanceOf<NoContentResult>());
     }
 
+    [Category("Sad Path")]
+    [Category("DeleteProduct")]
+    [Test]
+    public async Task DeleteProduct_WhenGivenInvalidID_ReturnsNotFound()
+    {
+        var mockService = new Mock<INorthwindService<Product>>();
+
+        mockService.Setup(sc => sc.DeleteAsync(It.IsAny<int>()))
+            .ReturnsAsync(false);
+
+        var sut = new ProductsController(mockService.Object);
+        var result = await sut.DeleteProduct(1);
+
+        mockService.Verify(s => s.DeleteAsync(1), Times.Once);
+        Assert.That(result, Is.InstanceOf<NotFoundResult>());
+    }
+
     [Category("Happy Path")]
     [Category("PutProduct")]
     [Test]
@@ -169,6 +181,53 @@ internal class ProductsControllerShould
 
         Assert.That(result.Value.ProductName, Is.EqualTo(prod.ProductName));
     }
+
+    [Category("Sad Path")]
+    [Category("PutProduct")]
+    [Test]
+    public async Task PutProduct_WhenGivenInvalidId_ReturnsNotFound()
+    {
+        var mockService = Mock.Of<INorthwindService<Product>>();
+        Product prod = new Product
+        {
+            ProductId = 1,
+            ProductName = "Shower Gel",
+            UnitsInStock = 5
+        };
+
+        Mock.Get(mockService)
+            .Setup(sc => sc.UpdateAsync(1, prod).Result)
+            .Returns(false);
+
+        var sut = new ProductsController(mockService);
+        var result = await sut.PutProduct(1, prod);
+
+
+
+        Assert.That(result, Is.InstanceOf<NotFoundResult>());
+    }
+
+    [Category("Sad Path")]
+    [Category("PutProduct")]
+    [Test]
+    public async Task PutProduct_WhenGivenMismatchingIds_ReturnsBadRequest()
+    {
+        var mockService = Mock.Of<INorthwindService<Product>>();
+        Product prod = new Product
+        {
+            ProductId = 1,
+            ProductName = "Shower Gel",
+            UnitsInStock = 5
+        };
+
+        var sut = new ProductsController(mockService);
+        var result = await sut.PutProduct(2, prod);
+
+        
+
+        Assert.That(result, Is.InstanceOf<BadRequestResult>());
+    }
+
     [Category("Happy Path")]
     [Category("PostSupplier")]
     [Test]
@@ -203,6 +262,42 @@ internal class ProductsControllerShould
         var result = await sut.GetProducts();
 
         Assert.That(result.Value, Is.InstanceOf<IEnumerable<ProductDTO>>());
+    }
+
+    [Category("Sad Path")]
+    [Category("PostSupplier")]
+    [Test]
+    public async Task PostSupplier_WhenGivenIdThatAlreadyExists_Ret()
+    {
+        var mockService = Mock.Of<INorthwindService<Product>>();
+
+        Product prod = new Product
+        {
+            ProductId = 1,
+            ProductName = "Shampoo",
+            UnitsInStock = 5
+        };
+
+        List<Product> suppliers = new List<Product>
+        {
+            new Product
+                {
+                    ProductId = 1,
+                    ProductName = "Shampoo",
+                    UnitsInStock = 5
+                }
+        };
+
+        Mock
+        .Get(mockService)
+        .Setup(sc => sc.GetAllAsync().Result)
+        .Returns(suppliers);
+
+        var sut = new ProductsController(mockService);
+        await sut.PostProduct(prod);
+        var result = await sut.GetProducts();
+
+        Assert.That(result.Value, Is.InstanceOf<ProblemHttpResult>());
     }
 
 }
